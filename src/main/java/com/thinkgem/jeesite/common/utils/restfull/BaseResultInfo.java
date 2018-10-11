69e177734dbc30204c689cd4ab4e2a403751a671
长春叭哥 <admin@chinadays.cn>
2018-10-11 22:01:00 +0800

    BaseResultInfo.java

---
+++ src/main/java/com/thinkgem/jeesite/common/utils/restfull/BaseResultInfo.java
@@ -0,0 +1,93 @@
+package com.thinkgem.jeesite.common.utils.restfull;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import java.util.Map;
+
+/**
+ * Restfull API Common ResultInfo class
+ * 
+ * @param <T>
+ *            返回数据
+ * @author yaohailu
+ * @version 2018年10月11日
+ */
+public class BaseResultInfo<T> implements Serializable {
+
+	protected static final long serialVersionUID = -2368446516546812379L;
+
+	/**
+	 * Result Data
+	 */
+	protected T data;
+	protected String code = ResultType.ERROR;
+	protected String msg;
+
+	/**
+	 * 获取默认结果集实例
+	 * 
+	 * @return
+	 * @author yaohailu
+	 */
+	public static Map<String, Object> getDefaultDataInstance() {
+		return new HashMap<String, Object>();
+	}
+
+	/**
+	 * 返回默认结果集
+	 * 
+	 * @param code
+	 * @param msg
+	 * @param data
+	 * @return
+	 * @author yaohailu
+	 */
+	public static BaseResultInfo<Map<String, Object>> getDefaultResultInfo(String code, String msg,
+			Map<String, Object> data) {
+		return new BaseResultInfo<Map<String, Object>>(ResultType.SUCCESS, "获取成功", data);
+	}
+
+	public BaseResultInfo() {
+
+	}
+
+	public BaseResultInfo(T t) {
+		setData(t);
+	}
+
+	public BaseResultInfo(String code, String msg) {
+		setCode(code);
+		setMsg(msg);
+	}
+
+	public BaseResultInfo(String code, String msg, T t) {
+		this(code, msg);
+		setData(t);
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+}
---
+++ src/main/java/com/thinkgem/jeesite/common/utils/restfull/ResultType.java
@@ -0,0 +1,27 @@
+package com.thinkgem.jeesite.common.utils.restfull;
+
+import java.io.Serializable;
+
+/**
+ * 返回结果类型
+ * 
+ * @author 长春叭哥
+ * @version 2018-10-12
+ */
+public class ResultType implements Serializable {
+
+	/**
+	 * Serializable
+	 */
+	private static final long serialVersionUID = 1450317220382328032L;
+
+	/**
+	 * 操作失败 0
+	 */
+	public final static String ERROR = "0";
+	/**
+	 * 操作成功 1
+	 */
+	public final static String SUCCESS = "1";
+
+}
